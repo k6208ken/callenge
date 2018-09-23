ae05ab41af439d415fae4efb04a8bffb9cac93cc
k6208ken <a618613915@gmail.com>
2018-09-23 17:14:29 +0800

    count_gain function done

--- ch6.py
+++ ch6.py
@@ -5,36 +5,55 @@ def stock_input(file_name):
 
 	stocks = []
 	with open(file_name) as csvfile:
-		rows = csv.reader(csvfile)
+		rows = csv.DictReader(csvfile)
 		for row in rows:
-			stocks.append(row[5].replace(" ",""))
-	stock = stocks[1:]
-	return stock	
+			integer = int(float(row['Adj Close']))
+			stocks.append(integer)
+	return stocks	
+
 
 def three_days(data):
-	result = []
+    output = []
+    for i in range(len(data)):
+        if i < 3:
+            output.append(0)
+        elif data[i] > data[i-1] > data[i-2] > data[i-3]:
+            output.append(1)
+        elif data[i] < data[i-1] < data[i-2] < data[i-3]:
+            output.append(-1)
+        else:
+            output.append(0)
+    return output
 
-	for i in range(len(data)):
-		if i < 3:
-			result.append(0)
+def count_gain(output,stock):
+	out = []
+	gain = 0
+	count = 0
+	for o in output:
+		count += o
+		print(count)
+		if count > 1:
+			out.append(0)
+		elif count < -1:
+			out.append(0)
 		else:
-			if data[i] > data[i-1] and data[i-1] > data[i-2] and data[i-2] > data[i-3]:
-				result.append(1)
-			elif data[i] < data[i-1] and data[i-1] < data[i-2] and data[i-2] < data[i-3]:
-				result.append(-1)
-			else:
-				result.append(0)
-	return result
-
-def count_gain(result,stock):
+			out.append(o)
+	print(out)
 	
-	gain = [i * j for i, j in zip(result, stock)]
-	print(gain)
+	for i, j in zip(out, stock):
+		gain += (i * j)
+	print ('總共獲利{}元'.format(gain * 1000))
+	
+	# gain = [i * j for i, j in zip(out, stock)]
+	
+
+
+	# print(gain)
 	
 
 	
 if __name__ == '__main__':
 	stock = stock_input('2330.csv')
-	result = three_days(stock)
-	count_gain(result,stock)
+	output = three_days(stock)
+	count_gain(output,stock)
 	
